18a5b0c50fa26c615ec61031b8afecac43afd679
赵哲 <754340156@qq.com>
2016-10-31 11:05:31 +0800

    初始化

---
+++ CloudPhotos/CloudPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudPhotos
+//
+//  Created by zhaozhe on 16/10/31.
+//  Copyright © 2016年 zhaozhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
